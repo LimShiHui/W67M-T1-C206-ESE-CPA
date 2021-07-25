e0bc080bb06e2c65e0fe6348314ee762e03afb74
huihu <20032316@myrp.edu.sg>
2021-07-25 19:32:55 +0800

    add career class

---
+++ C206_CaseStudy/src/career.java
@@ -0,0 +1,21 @@
+
+public class career {
+	
+	private int career_id;
+	private String career_name;
+	
+	public career(int career_id, String career_name) {
+		super();
+		this.career_id = career_id;
+		this.career_name = career_name;
+	}
+
+	public int getCareer_id() {
+		return career_id;
+	}
+
+	public String getCareer_name() {
+		return career_name;
+	}
+
+}
